0be059bcd4923605c6fe92d743c29ed104972506
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-04-02 09:47:40 +0200

    3 min time limit

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 SOLVER_DIR="${HOME}/dependency-curated-lns-gecode"
 SOLVER="${SOLVER_DIR}/build/tools/flatzinc/gecode.msc"
 EXTRA="--extra --solver Dexter -a"
-TIME_LIMIT=300000
+TIME_LIMIT=180000
 NUM_RUNS=10
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
