e8759a0ac45430a171f308528eca701b671554c1
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2026-09-03 09:53:32 +0200

    fixed jobshop structure

--- run.sh
+++ run.sh
@@ -17,54 +17,54 @@ true \
 true)
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 declare -a ACTIVE_PROBLEMS=( 0 1 2 3 4 5 )
-declare -a FILE_NAMES=(\
+declare -a PROBLEMS=(\
+"hospital-residents-with-couples" \
 "jobshop" \
-"tsptw" \
+"knapsack" \
+"rcpsp" \
+"rcpsp-wet" \
 "steelmillslab" \
-"rotating-workforce" \
+"tdptw" \
+"tsptw" \
+"vrp" \
+"carseq" \
 "nurse-rostering" \
-"carseq")
-declare -a FOLDERS=(
-"${SCRIPT_DIR}/jobshop/" \
-"${SCRIPT_DIR}/tsptw/" \
-"${SCRIPT_DIR}/steelmillslab/" \
-"${SCRIPT_DIR}/rotating-workforce/" \
-"${SCRIPT_DIR}/nurse-rostering/" \
-"${SCRIPT_DIR}/carseq/")
-declare -a DATA_LOCATIONS=(\
-"${SCRIPT_DIR}/jobshop/dzn/jobshop_swv*-10.dzn" \
-"${SCRIPT_DIR}/tsptw/dzn/*.003.dzn" \
-"${SCRIPT_DIR}/steelmillslab/dzn/bench_20_*.dzn" \
-"${SCRIPT_DIR}/rotating-workforce/dzn/Example3*.dzn" \
-"${SCRIPT_DIR}/nurse-rostering/dzn/*.dzn" \
-"${SCRIPT_DIR}/carseq/dzn/car*1*dzn")
+"rotating-workforce")
 declare -a IS_CSP=(\
-false \
-false \
-false \
-true \
-true \
-true)
-#  3 min timeout for COP
+0 \
+0 \
+0 \
+0 \
+0 \
+0 \
+0 \
+0 \
+0 \
+1 \
+1 \
+1)
 # 30 min timeout for CSP
-# TIME_LIMITS=(180000 180000 180000 180000 180000 180000 2700000 2700000 2700000)
-TIME_LIMITS=(180000 180000 180000 180000 180000 180000 180000 180000 180000)
+#  3 min timeout for COP
+# declare -a TIME_LIMITS=(2700000 180000)
+#  3 min timeout for both:
+declare -a TIME_LIMITS=(180000 180000)
 for s in "${!SOLVERS[@]}"; do
   SOLVER=${SOLVERS[$s]}
   SUFFIX=${SUFFIXES[$s]}
   RUNS=${NUM_RUNS[$s]}
   EXTRA=${FLAGS[$s]}
   for i in "${ACTIVE_PROBLEMS[@]}"; do
-    if [ "${IS_CSP[$i]}" = true ] && [ "${HANDLES_CSP[$s]}" = false ];
+    CSP=${IS_CSP[$i]}
+    if [ "${IS_CSP[$i]}" = 1 ] && [ "${HANDLES_CSP[$s]}" = false ];
     then
         continue
     fi
-    FILE_NAME=${FILE_NAMES[$i]}
-    MZN="${FOLDERS[$i]}${FILE_NAME}.mzn"
-    DATA=${DATA_LOCATIONS[$i]}
-    OUTPUT="${SCRIPT_DIR}/results/${FILE_NAME}.txt-${SUFFIX}"
-    TIME_LIMIT=${TIME_LIMITS[$i]}
-    if [ ${IS_CSP[$i]} = true ]; then CSP_FLAG="--csp"; else CSP_FLAG=""; fi
+    PROBLEM=${PROBLEMS[$i]}
+    MZN="${SCRIPT_DIR}/${PROBLEM}/${PROBLEM}.mzn"
+    DATA="${SCRIPT_DIR}/${PROBLEM}/dzn/*.dzn"
+    OUTPUT="${SCRIPT_DIR}/results/${PROBLEM}.txt-${SUFFIX}"
+    TIME_LIMIT=${TIME_LIMITS[$CSP]}
+    if [ ${IS_CSP[$i]} = 1 ]; then CSP_FLAG="--csp"; else CSP_FLAG=""; fi
     echo "--solver ${SOLVER}"
     echo "         ${MZN}"
     echo "         -d ${DATA}"
@@ -73,6 +73,7 @@ for s in "${!SOLVERS[@]}"; do
     echo "         --num-runs ${RUNS}"
     echo "         ${CSP_FLAG}"
     echo "         ${EXTRA}"
+    continue
     python3 run.py --solver ${SOLVER} \
                    ${MZN} \
                    -d ${DATA} \
